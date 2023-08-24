83e245939a178f919e58c240121f12dbc1cb4f29
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-24 21:00:55 +0100

    Write a script that lists all the tables of a database in your MySQL server.

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,9 +1,4 @@
 -- This script lists all the tables of a database
 
--- The USE statement selects the current database
-
-USE database_name;
-
 -- The SHOW TABLES statement lists all the tables in the database
-
 SHOW TABLES;
