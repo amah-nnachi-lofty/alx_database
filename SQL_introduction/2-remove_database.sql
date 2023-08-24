02abd59e881df82172234f4f2c9627f5e055befa
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-24 20:19:40 +0100

    Write a script that deletes the database hbtn_0c_0 in your MySQL server

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -3,7 +3,7 @@
 -- Set the name of the database you want to delete
 SET @db_name = 'hbtn_0c_0';
 
--- Create a temporary variable to hold the result of DROP DATABASE
+-- Create a temporary variable to store the result of DROP DATABASE
 SET @drop_result = '';
 
 -- Attempt to drop the database if it exists
@@ -12,11 +12,10 @@ DROP DATABASE IF EXISTS hbtn_0c_0;
 
 -- Attempt to drop the database again and store the result in the variable
 BEGIN
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '42000'
     BEGIN
         SET @drop_result = 'Database did not exist. Nothing to delete.';
     END;
-    SET @dummy = (SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'hbtn_0c_0');
     DROP DATABASE hbtn_0c_0;
     SET @drop_result = 'Database has been deleted.';
 END;
