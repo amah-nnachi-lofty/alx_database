c498e8eb017878e110cd64f8c56c3e332c192d41
Amah Nnachi Lofty <loftdot@gmail.com>
2023-09-05 09:56:31 +0100

    Write a script that lists all cities contained in the database hbtn_0d_usa.
        Each record should display: cities.id - cities.name - states.name
        Results must be sorted in ascending order by cities.id
        You can use only one SELECT statement
        The database name will be passed as an argument of the mysql command

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 -- Write a script that lists all cities contained in the database hbtn_0d_usa
-    --Each record should display
-    -- Results must be sorted in ascending order by cities.id
-    -- You can use only one SELECT statement
-    -- The database name will be passed as an argument of the mysql command
 
 CREATE DATABASE hbtn_0d_usa;
 
